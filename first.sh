ed3d25a3cd9e1ec85d110808202cb353c04146eb
CyberLegion <142536803+CyberLegion@users.noreply.github.com>
2023-08-18 00:37:14 +0530

    Add files via upload

---
+++ first.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+mkdir hello
+echo "Created Folder Successfully"
+echo " "
+echo " "
+cd hello
+echo "Type Your Content In the New Text file : "
+echo " "
+cat >new.txt
+echo "\n>>>File Created Successfully"
+clear
+echo ">>Your Text File Contents :  "
+echo " "
+cat new.txt
+
